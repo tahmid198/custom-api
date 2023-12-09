03c425895e16ba497cda991fe511ac779182895f
Tahmid Zaman <tahmid198@yahoo.com>
2023-12-09 06:15:32 -0500

    able to retrive data from server using uuid

--- routes/users.js
+++ routes/users.js
@@ -21,10 +21,16 @@ router.get('/', (req, res) => {
 router.post('/', (req, res) => {
     
     const user = req.body;
-
     users.push({... user, id: uuidv4()});
-
     res.send(`User with the username ${user.first_name} added to the database!`);
+
+});
+
+router.get('/:id', (req, res) =>{
+
+    const { id } = req.params;
+    const foundUser = users.find((user) => user.id === id);
+    res.send(foundUser);
 
 });
 
